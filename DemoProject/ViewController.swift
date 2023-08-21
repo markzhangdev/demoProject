77222ea2a6defad43fa848fefe91b06138a24942
Ming Zhang <ming.zhang@thelotterycorporation.com>
2023-08-21 10:39:54 +1000

    Setup an empty project

---
+++ DemoProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoProject
+//
+//  Created by Zhang, Mark on 21/8/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
